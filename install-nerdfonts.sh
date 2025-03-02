add1d2df1e158311bf47292065499fdc81026fcd
bruno@loka <bruno@loka.com>
2025-03-02 01:54:13 +0000

    install nerd fonts

---
+++ install-nerdfonts.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# expects that we are running on a
+# dedicated directory
+
+FONTS_DIR="/usr/share/fonts"
+
+# Check if fonts directory exists
+if [ ! -d "$FONTS_DIR" ]; then
+  echo "Fonts directory does not exist. Creating it now..."
+  mkdir -p "$FONTS_DIR"
+  echo "Directory created: $FONTS_DIR"
+else
+  echo "Directory already exists: $FONTS_DIR"
+fi
+
+wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/BigBlueTerminal.zip"
+unzip BigBlueTerminal.zip -d "BigBlueTerminal"
+rm BigBlueTerminal.zip
+cd BigBlueTerminal
+cp *.ttf "$FONTS_DIR"
