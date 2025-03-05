a455578c80b5d48134235f43524714b1c146ccb5
bruno@loka <bruno@loka.com>
2025-03-05 00:04:37 +0000

    version fix: added gawk

--- version-check-fix.sh
+++ version-check-fix.sh
@@ -1,5 +1,6 @@
-# fixing missing softawre on Ubuntu 24.10 server host OS
+# fixing missing softawre on Debian
 #
 sudo apt install bison
 sudo apt install build-essential
 sudo apt install texinfo
+sudo apt install gawk
